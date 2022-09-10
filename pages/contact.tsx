2a2456071c168f476b06179e15063232791c9484
Dan Groleau <ddgroleau@gmail.com>
2022-09-10 10:44:53 -0400

    Add contact page.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import React from 'react';
+import ContactForm from '../components/ContactForm';
 import Layout from '../components/Layout';
+import StyledLink from '../components/StyledLink';
 import styles from '../styles/Contact.module.scss';
 
 const Contact = () => {
@@ -11,7 +14,42 @@ const Contact = () => {
         ThoughSpring Creative LLC. is a veteran-owned software development company located in Portland, ME, 
         and proudly serves businesses of any size across MA, CT, NH, VT, RI and ME.`}>
             <div className={styles.container}>
-                
+                <section className={styles.hero}>
+                    <div className={styles.heroImg}>
+                        <div className={styles.heroContent}>
+                            <div className={styles.flexWrapper}>
+                                <div className={styles.text}>
+                                    <h1 className={styles.title}>We are here for you.</h1>
+                                    <p className={styles.main}>
+                                        Thanks for your interest in hiring 
+                                        ThoughtSpring Creative to build your next project. 
+                                        Every client that works with us gets our undivided attention.
+                                    </p>
+                                    <p className={styles.supporting}>
+                                        You can also email us 
+                                        at <Link href="mailto:email@thoughtspringcreative.com">
+                                            email@thoughtspringcreative.com
+                                        </Link>, or 
+                                        call us at <Link href="tel:5555555555">(555) 555 - 5555</Link> to get started.
+                                    </p>
+                                </div>
+                                <div className={styles.form}>
+                                    <ContactForm/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className={styles.body}>
+                    <h2>Ready to get started?</h2>
+                    <StyledLink 
+                        innerText="SCHEDULE A CONSULTATION" 
+                        backgroundColor={styles.blue} 
+                        color={styles.white} 
+                        fontSize={"x-large"}
+                        href="/contact"
+                    />
+                </section>
             </div>
         </Layout>
     );
