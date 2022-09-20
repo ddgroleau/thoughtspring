599330b7282bc6e3a43025c7d29768ae75b77b0e
Dan Groleau <ddgroleau@gmail.com>
2022-09-20 14:13:34 -0400

    Add SMTP Port.

--- services/EmailMessenger.ts
+++ services/EmailMessenger.ts
@@ -7,6 +7,8 @@ export default class EmailMessenger {
 
         const smtpclient:nodemailer.Transporter = nodemailer.createTransport({
             service: 'gmail',
+            port: parseInt(process.env.SMTP_PORT as string),
+            secure: true,
             auth: {
                 user: process.env.EMAIL_ACCOUNT,
                 pass: process.env.EMAIL_PASSWORD,
